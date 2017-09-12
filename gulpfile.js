5a785c9d1b3d6e9afd3623b4f23e55688499d9b4
Curtis Timson <curtis.timson@whitbread.com>
2017-09-12 16:35:49 +0100

    :sparkles: Create gulp watcher for main-sass and add to default task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('default', function(){
+gulp.task('default', ['sass-main-watch']);
 
+gulp.task('sass-main-watch', function(){
+  gulp.watch('./static/assets/sass/main.scss', ['sass-main']);
 });
 
 gulp.task('sass-main', function(){
