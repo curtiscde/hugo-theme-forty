73fbb393d01179a7aa8d41f2f13ac8fd827641ed
Curtis Timson <curtis.timson@whitbread.com>
2017-09-12 16:48:55 +0100

    :sparkles: Add gulp tasks for ie8 and ie9 sass files

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('default', ['sass-main-watch']);
+gulp.task('default', ['sass-main-watch', 'sass-ie8-watch', 'sass-ie9-watch']);
 
 gulp.task('sass-main-watch', function(){
   gulp.watch('./static/assets/sass/main.scss', ['sass-main']);
@@ -9,6 +9,30 @@ gulp.task('sass-main-watch', function(){
 
 gulp.task('sass-main', function(){
   return gulp.src('./static/assets/sass/main.scss')
+              .pipe(sass().on('error', sass.logError))
+              .pipe(gulp.dest('./static/assets/css'));
+});
+
+
+
+gulp.task('sass-ie8-watch', function(){
+  gulp.watch('./static/assets/sass/ie8.scss', ['sass-ie8']);
+});
+
+gulp.task('sass-ie8', function(){
+  return gulp.src('./static/assets/sass/ie8.scss')
+              .pipe(sass().on('error', sass.logError))
+              .pipe(gulp.dest('./static/assets/css'));
+});
+
+
+
+gulp.task('sass-ie9-watch', function(){
+  gulp.watch('./static/assets/sass/ie9.scss', ['sass-ie9']);
+});
+
+gulp.task('sass-ie9', function(){
+  return gulp.src('./static/assets/sass/ie9.scss')
               .pipe(sass().on('error', sass.logError))
               .pipe(gulp.dest('./static/assets/css'));
 });
