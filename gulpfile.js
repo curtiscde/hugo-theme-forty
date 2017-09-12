9804832878d8cd13f22b902aa0ec0be27355f4c6
Curtis Timson <curtis.timson@whitbread.com>
2017-09-12 16:28:42 +0100

    :sparkles: Add gulp task to transpile main.scss

--- gulpfile.js
+++ gulpfile.js
@@ -4,3 +4,9 @@ var sass = require('gulp-sass');
 gulp.task('default', function(){
 
 });
+
+gulp.task('sass-main', function(){
+  return gulp.src('./static/assets/sass/main.scss')
+              .pipe(sass().on('error', sass.logError))
+              .pipe(gulp.dest('./static/assets/css'));
+});
